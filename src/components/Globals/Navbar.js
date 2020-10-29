84f0a55aab5feba5cd3d5c6294e1837d69daafd3
MP <markpaololibunao@gmail.com>
2020-10-29 14:24:28 +0800

    Navbar added as well as basic css

---
+++ src/components/Globals/Navbar.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import logo from "../../images/logo.svg"
+import { FaCartArrowDown } from "react-icons/fa"
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+  const [css, setCss] = useState("collapse navbar-collapse")
+  const [links, setLinks] = useState([
+    { id: 1, path: "/", text: "home" },
+    { id: 2, path: "/about", text: "About" },
+  ])
+
+  const navbarHandler = () => {
+    if (navbarOpen) {
+      setNavbarOpen(false)
+      setCss("collapse navbar-collapse")
+    } else {
+      setNavbarOpen(true)
+      setCss("collapse navbar-collapse show")
+    }
+  }
+
+  return (
+    <nav className="navbar navbar-expand-sm bg-light navbar-light">
+      <Link to="/" className="navbar-brand">
+        <img src={logo} alt="logo" />
+        {/* https://www.iconfinder.com/icons/185113/coffee_streamline_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/webalys */}
+      </Link>
+      <button className="navbar-toggler" type="button" onClick={navbarHandler}>
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className={css}>
+        <ul className="navbar-nav mx-auto">
+          {links.map(link => {
+            return (
+              <li key={link.id} className="nav-item">
+                <Link to={link.path} className="nav-link text-capitalize">
+                  {link.text}
+                </Link>
+              </li>
+            )
+          })}
+          <li className="nav-item ml-sm-5">
+            <FaCartArrowDown className="cart-icon" />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,13 +7,20 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import "./bootstrap.min.css"
 import { useStaticQuery, graphql } from "gatsby"
 
+import "./bootstrap.min.css"
 import "./layout.css"
 
+import Navbar from "./Globals/Navbar"
+
 const Layout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  )
 }
 
 Layout.propTypes = {
